afe8e8d61bafe5225963efe32e16340792c4ac80
cw417 <chriswhitlock417@gmail.com>
2023-03-07 04:41:44 -0500

    Add accountType toggle; add transaction logging; add transfer page navigate

--- client/src/views/CustomerPage.js
+++ client/src/views/CustomerPage.js
@@ -1,26 +1,45 @@
 import { useRef, useState } from 'react'
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import TransactionHistory from '../components/TransactionHistory';
 
 export default function CustomerPage({}) {
 
   // get the customer from React Router
   const location = useLocation();
-  const params = useParams();
   const [customer, setCustomer] = useState(location.state.customer);
-
+  const [accountType, setAccountType] = useState('chequing');
+  const navigate = useNavigate();
   const depositRef = useRef();
-
-  function deposit() {
+  
+  function addToChequing(amount) {
     // add funds to selected account
     // add transaction log to transactionHistory
-    const newCustomer = {...customer};
-    newCustomer.accounts.chequing += parseInt(depositRef.current.value);
+    const newCustomer = { ...customer };
+    newCustomer.accounts.chequing += amount;
+    newCustomer.transactionHistory.push({amount: amount, accountType: 'Chequing', to: 'Deposit', from: 'Deposit'});
     setCustomer(newCustomer);
-    handleSubmit();
   }
-  
-  async function handleSubmit() {
+
+  function addToSavings(amount) {
+    // add funds to selected account
+    // add transaction log to transactionHistory
+    const newCustomer = { ...customer };
+    newCustomer.accounts.savings += amount;
+    newCustomer.transactionHistory.push({amount: amount, accountType: 'Savings', to: 'Deposit', from: 'Deposit'});
+    setCustomer(newCustomer);
+  }
+
+  async function deposit() {
+    const amount = parseInt(depositRef.current.value);
+    console.log(accountType)
+    if (accountType == 'chequing') {
+      addToChequing(amount);
+    }
+    else {
+      addToSavings(amount);
+    }
+    depositRef.current.value = null;
     const updatedCustomer = {...customer};
     await fetch(`http://localhost:5000/update/${customer._id}`, {
       method: "POST",
@@ -28,9 +47,17 @@ export default function CustomerPage({}) {
       headers: {
         'Content-Type': 'application/json'
       },
-    });
+    })
   }
 
+  function toTransfer() {
+    navigate('/transfer', {state: { customer: customer } });
+  }
+
+  function handleChange(event) {
+    setAccountType(event.target.value);
+    console.log(accountType);
+  }
 
   return (
     <div>
@@ -45,8 +72,18 @@ export default function CustomerPage({}) {
 
         <div>
           <input type='text' ref={depositRef}/>
-          <button type='submit' onClick={deposit}>+</button>
+          <input type='radio' id='chequing' name='accountType' value='chequing' onChange={handleChange} />
+          <label>Chequing</label>
+          <input type='radio' id='savings' name='accountType' value='savings' onChange={handleChange} />
+          <label>Savings</label>
+          <button type='submit' onClick={deposit}>Deposit</button>
         </div>
+
+        <div>
+          <button onClick={toTransfer}>Make Transfer</button>
+        </div>
+
+        <TransactionHistory customer={customer} />
       </div>
     </div>
   )
