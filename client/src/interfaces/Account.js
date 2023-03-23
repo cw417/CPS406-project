69dc4f30e6956c10c0111fdcba30a5e4ab051ace
SpringCoooler <zxz9393@gmail.com>
2023-03-23 00:14:30 -0400

    interfaces/account.js and transaction.js
    yolo!

--- client/src/interfaces/Account.js
+++ client/src/interfaces/Account.js
@@ -1,57 +1,55 @@
 export default class Account {
-    constructor(username, first, last, address, email, password, chequing, savings, id) {
-      this.id = id;
-      this.username = username;
-      this.first = first;
-      this.last = last;
-      this.address = address;
-      this.email = email;
-      this.password = password;
-      this.accounts = {
-        chequing: chequing,
-        savings: savings
-      }
-    }
-  
-    getId() {
-      return this.id;
-    }
-  
-    getInfo() {
-      return {username: this.username, first: this.first, last: this.last, address: this.address, email: this.email};
-    }
-  
-    getChequing() {
-      return this.accounts.chequing;
-    }
-  
-    getSavings() {
-      return this.accounts.savings;
-    }
-  
-    getId() {
-      return this.id;
-    }
-  
-    async updateCustomer() {
-      await fetch(`http://localhost:5000/update/${this.id}`, {
-        method: "POST",
-        body: JSON.stringify({
-          username: this.username,
-          first: this.first,
-          last: this.last,
-          address: this.address,
-          email: this.email,
-          password: this.password,
-          accounts: {
-            chequing: this.accounts.chequing,
-            savings: this.accounts.savings
-          }
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-  
-    }
+  constructor(
+    id,
+    accountType,
+    customerId,
+    accountBalance,
+    maxTransferAccount,
+    transactionHistory
+  ) {
+    this.id = id;
+    this.accountType = accountType;
+    this.customerId = customerId;
+    this.accountBalance = accountBalance;
+    this.maxTransferAccount = maxTransferAccount;
+    this.transactionHistory = transactionHistory;
   }
+
+  setAccountBalance(newVal) {
+    this.accountBalance = newVal;
+  }
+  
+  addTransaction(transaction) { 
+    this.transactionHistory.push(transaction);
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getInfo() {
+    return {
+      accountType: this.accountType,
+      customerId: this.customerId,
+      accountBalance: this.accountBalance,
+      maxTransferAccount: this.maxTransferAccount,
+      transactionHistory: this.transactionHistory,
+    };
+  }
+
+  async updateAccount() {
+    await fetch(`http://localhost:5000/account/update/${this.id}`, {
+      method: "POST",
+      body: JSON.stringify({
+        accountType: this.accountType,
+        customerId: this.customerId,
+        accountBalance: this.accountBalance,
+        maxTransferAccount: this.maxTransferAccount,
+        transactionHistory: this.transactionHistory,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
--- client/src/interfaces/Transaction.js
+++ client/src/interfaces/Transaction.js
@@ -1,11 +1,20 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuid4 } from 'uuid';
 
-export default class Transaction{
+export default class Transaction {
   constructor(amount, accountType, to, from) {
-    this.id = uuidv4();
+    this.id = uuid4();
     this.amount = amount;
     this.accountType = accountType;
     this.to = to;
     this.from = from;
+  }
+
+  getInfo() {
+    return {
+      amount: this.amount,
+      accountType: this.accountType,
+      to: this.to,
+      from: this.from,
+    };
   }
 }
