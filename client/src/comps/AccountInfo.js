1bf98d85863289b0d3d496edcf19accab63b43ac
GurkarnSD <GurkarnSD@outlook.com>
2023-03-24 21:12:33 -0400

    Updated AccountInfo Comp To Display Transactions

--- client/src/comps/AccountInfo.js
+++ client/src/comps/AccountInfo.js
@@ -1,5 +1,6 @@
 import Bank from "../objects/Bank"
 import { useState, useEffect } from 'react'
+import styles from '../styles/AccountInfo.module.css'
 
 export default function AccountInfo(props) {
     const accountId = props.accountId
@@ -24,8 +25,35 @@ export default function AccountInfo(props) {
 
     return(
         <>
-            <p>AccountInfo</p>
-            <p>{account.accountBalance}</p>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <p>{account.accountType} Account - {account._id}</p>
+                    <p>Current Balance: ${account.accountBalance}</p>
+                </div>
+                <hr/>
+                <div className={styles.header}>
+                    <p>Date</p>
+                    <p>To</p>
+                    <p>From</p>
+                    <p>Description</p>
+                    <p>Amount</p>
+                </div>
+                <div className={styles.body}>
+                    {account.transactionHistory.map((transaction) => {
+                        return(
+                            <>
+                                <div className={styles.transaction}>
+                                    <p>{transaction.date}</p>
+                                    <p>{transaction.to}</p>
+                                    <p>{transaction.from}</p>
+                                    <p>{transaction.type}</p>
+                                    <p>{transaction.amount}</p>
+                                </div>
+                            </>
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
