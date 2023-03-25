1e88e1081d9c06490c7da3ca14466ef89cc6502a
SpringCoooler <zxz9393@gmail.com>
2023-03-25 16:04:51 -0400

    Transfer.js

--- client/src/comps/Transfer.js
+++ client/src/comps/Transfer.js
@@ -1,6 +1,6 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import DropdownStyles from "../styles/DropdownMenu.module.css";
-import styles from "../styles/Transfer.module.css";
+import TransferStyles from "../styles/Transfer.module.css";
 import { useRef, useState } from "react";
 import Transaction from "../objects/Transaction";
 
@@ -43,70 +43,89 @@ export default function Transfer(props) {
 
   return (
     <>
-      <div>
-        <span>From: </span>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
-            {displayFromAccount}
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content
-            className={DropdownStyles.Content}
-            align="start"
-          >
-            {accounts.map((account) => {
-              return (
-                <DropdownMenu.Item
-                  className={DropdownStyles.Item}
-                  onSelect={() => {
-                    setFromAccount(account);
-                    setDisplayFromAccount(account.id);
-                  }}
-                >
-                  <p>
-                    {account.accountType} Account - {account.id}
-                  </p>
-                  <p>{account.accountBalance}</p>
-                </DropdownMenu.Item>
-              );
-            })}
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-      </div>
-      <div>
-        <span>To: </span>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
-            {displayToAccount}
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content
-            className={DropdownStyles.Content}
-            align="start"
-          >
-            {accounts.map((account) => {
-              return (
-                <DropdownMenu.Item
-                  className={DropdownStyles.Item}
-                  onSelect={() => {
-                    setToAccount(account);
-                    setDisplayToAccount(account.id);
-                  }}
-                >
-                  <p>
-                    {account.accountType} Account - {account.id}
-                  </p>
-                  <p>{account.accountBalance}</p>
-                </DropdownMenu.Item>
-              );
-            })}
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-      </div>
-
-      <div>
-        <label>Amount: </label>
-        <input ref={amountRef} type="text" />
-        <div>
-          <button onClick={handleTransfer}>Transfer</button>{" "}
+      <div className={TransferStyles.Form}>
+        <div className={TransferStyles.FormElement}>
+          <div>From: </div>
+          <div>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
+                {displayFromAccount}
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content
+                className={DropdownStyles.Content}
+                align="start"
+              >
+                {accounts.map((account) => {
+                  return (
+                    <DropdownMenu.Item
+                      className={DropdownStyles.Item}
+                      onSelect={() => {
+                        setFromAccount(account);
+                        setDisplayFromAccount(account.id);
+                      }}
+                    >
+                      <p>
+                        {account.accountType} Account - {account.id}
+                      </p>
+                      <p>{account.accountBalance}</p>
+                    </DropdownMenu.Item>
+                  );
+                })}
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          </div>
+        </div>
+        <div className={TransferStyles.FormElement}>
+          <div>To: </div>
+          <div>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
+                {displayToAccount}
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content
+                className={DropdownStyles.Content}
+                align="start"
+              >
+                {accounts.map((account) => {
+                  return (
+                    <DropdownMenu.Item
+                      className={DropdownStyles.Item}
+                      onSelect={() => {
+                        setToAccount(account);
+                        setDisplayToAccount(account.id);
+                      }}
+                    >
+                      <p>
+                        {account.accountType} Account - {account.id}
+                      </p>
+                      <p>{account.accountBalance}</p>
+                    </DropdownMenu.Item>
+                  );
+                })}
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          </div>
+        </div>
+        <div className={TransferStyles.FormElement}>
+          <div>
+            <label>Amount: </label>
+          </div>
+          <div>$
+            <input
+              ref={amountRef}
+              type="text"
+              className={TransferStyles.FormInput}
+              placeholder="0.00"
+            />
+          </div>
+          <div>
+            <button
+              onClick={handleTransfer}
+              className={TransferStyles.FormButton}
+            >
+              Transfer
+            </button>{" "}
+          </div>
         </div>
       </div>
     </>
