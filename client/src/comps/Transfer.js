e0d038583016a4aa3dfd3d6380121dfc90645b5c
SpringCoooler <zxz9393@gmail.com>
2023-03-25 00:35:05 -0400

    Update Transfer.js

--- client/src/comps/Transfer.js
+++ client/src/comps/Transfer.js
@@ -1,95 +1,94 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import DropdownStyles from "../styles/DropdownMenu.module.css";
-import TransferStyles from "../styles/Transfer.module.css";
 import { useRef, useState } from "react";
 import Transaction from "../objects/Transaction";
 
 export default function Transfer(props) {
+  const [fromAccount, setFromAccount] = useState(null);
+  const [displayFromAccount, setDisplayFromAccount] =
+    useState("Select Account");
+  const [toAccount, setToAccount] = useState(null);
+  const [displayToAccount, setDisplayToAccount] = useState("Select Account");
+  const sAccounts = props.sAccounts;
+  const cAccounts = props.cAccounts;
+  const accounts = sAccounts.concat(cAccounts);
+  const amountRef = useRef();
 
-    const [fromAccount, setFromAccount] = useState(null);
-    const [selectedFromAccount, setSelectedFromAccount] = useState('Select Account');
-    const [toAccount, setToAccount] = useState(null);
-    const [selectedToAccount, setSelectedToAccount] = useState('Select Account');
-    const sAccounts = props.sAccounts
-    const cAccounts = props.cAccounts
-    const accounts = sAccounts.concat(cAccounts)
-    const amountRef = useRef();
-
-    function handleTransfer() {
-        const amount = amountRef.current.value;
-        if (amount > 0 && fromAccount !== null && toAccount !== null && fromAccount !== toAccount) {
-            const fromTransaction = new Transaction(amount, fromAccount.accountType, toAccount.id, fromAccount.id, "Transfer");
-            const toTransaction = new Transaction(amount, toAccount.accountType, toAccount.id, fromAccount.id, "Transfer");
-            fromAccount.transfer(toAccount, amount, fromTransaction, toTransaction);
-        }
+  function handleTransfer() {
+    const amount = amountRef.current.value;
+    if (
+      amount > 0 &&
+      fromAccount !== null &&
+      toAccount !== null &&
+      fromAccount !== toAccount
+    ) {
+      const fromTransaction = new Transaction(
+        amount,
+        fromAccount.accountType,
+        toAccount.id,
+        fromAccount.id,
+        "Transfer"
+      );
+      const toTransaction = new Transaction(
+        amount,
+        toAccount.accountType,
+        toAccount.id,
+        fromAccount.id,
+        "Transfer"
+      );
+      fromAccount.transfer(toAccount, amount, fromTransaction, toTransaction);
     }
+  }
 
   return (
     <>
-      <div>
-        <h1>Transfer</h1>
-      </div>
-      <div>
-        <span>From: </span>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
-            {selectedFromAccount
-              ? `${selectedFromAccount.accountType} Account - ${selectedFromAccount.id}`
-              : "{displayFromAccount"}
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content
-            className={DropdownStyles.Content}
-            align="start"
-          >
-            {accounts.map((account) => {
-              return (
-                <DropdownMenu.Item
-                  className={DropdownStyles.Item}
-                  onSelect={() => {
-                    setFromAccount(account); setSelectedFromAccount(account.id);
-                  }}
-                >
-                  <p>
-                    {account.accountType} Account - {account.id}
-                  </p>
-                  <p>{account.accountBalance}</p>
-                </DropdownMenu.Item>
-              );
-            })}
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-      </div>
-      <div>
-        <span>To: </span>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
-            {selectedToAccount
-              ? `${selectedToAccount.accountType} Account - ${selectedToAccount.id}`
-              : "{displayToAccount"}
-          </DropdownMenu.Trigger>
-          <DropdownMenu.Content
-            className={DropdownStyles.Content}
-            align="start"
-          >
-            {accounts.map((account) => {
-              return (
-                <DropdownMenu.Item
-                  className={DropdownStyles.Item}
-                  onSelect={() => {
-                    setToAccount(account); setSelectedToAccount(account.id);
-                  }}
-                >
-                  <p>
-                    {account.accountType} Account - {account.id}
-                  </p>
-                  <p>{account.accountBalance}</p>
-                </DropdownMenu.Item>
-              );
-            })}
-          </DropdownMenu.Content>
-        </DropdownMenu.Root>
-      </div>
-
+      <h1>Transfer</h1>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
+          From: {displayFromAccount}
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content className={DropdownStyles.Content} align="start">
+          {accounts.map((account) => {
+            return (
+              <DropdownMenu.Item
+                className={DropdownStyles.Item}
+                onSelect={() => {
+                  setFromAccount(account);
+                  setDisplayFromAccount(account.id);
+                }}
+              >
+                <p>
+                  {account.accountType} Account - {account.id}
+                </p>
+                <p>{account.accountBalance}</p>
+              </DropdownMenu.Item>
+            );
+          })}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger className={DropdownStyles.Trigger}>
+          To: {displayToAccount}
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content className={DropdownStyles.Content} align="start">
+          {accounts.map((account) => {
+            return (
+              <DropdownMenu.Item
+                className={DropdownStyles.Item}
+                onSelect={() => {
+                  setToAccount(account);
+                  setDisplayToAccount(account.id);
+                }}
+              >
+                <p>
+                  {account.accountType} Account - {account.id}
+                </p>
+                <p>{account.accountBalance}</p>
+              </DropdownMenu.Item>
+            );
+          })}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
       <div>
         <label>Amount: </label>
         <input ref={amountRef} type="text" />
