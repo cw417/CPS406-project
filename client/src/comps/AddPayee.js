fb603f04992dab9a6eb46a0c6622cf13fed54a08
SpringCoooler <zxz9393@gmail.com>
2023-03-24 23:52:32 -0400

    'add payee' is done ( backend connection needed)

---
+++ client/src/comps/AddPayee.js
@@ -0,0 +1,50 @@
+import styles from "../styles/AddRecipient.module.css";
+import { useState } from "react";
+
+export default function AddPayee() {
+  const [payeeName, setPayeeName] = useState("");
+  const [accountNumber, setAccountNumber] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!/^[-a-zA-Z\s]+$/.test(payeeName)) {
+      alert(
+        "Invalid payee name. Payee name can only contain letters, spaces, and hyphens."
+      );
+      return;
+    }
+    if (!/^\d+$/.test(accountNumber)) {
+      alert("Invalid account number. Account number can only contain digits.");
+      return;
+    }
+  };
+
+  return (
+    <div className={styles.outerFrame}>
+      <div style={{ textAlign: "center" }}>
+        <p style={{ fontWeight: "bold", fontSize: "30px" }}>Add Payee</p>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Payee Name:
+            <input
+              type="text"
+              value={payeeName}
+              onChange={(e) => setPayeeName(e.target.value)}
+            />
+          </label>
+          <label>
+            Account Number:
+            <input
+              type="text"
+              value={accountNumber}
+              onChange={(e) => setAccountNumber(e.target.value)}
+            />
+          </label>
+          <button type="submit">Complete</button>
+        </form>
+      </div>
+    </div>
+  );
+}
