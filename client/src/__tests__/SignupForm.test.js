a4654ac316fb183f1ec3fccabb19bf23bc34e54d
Saydur Rahman <rahmansaydur511@gmail.com>
2023-03-28 16:59:11 -0400

    added more tests to Singup test file

--- client/src/__tests__/SignupForm.test.js
+++ client/src/__tests__/SignupForm.test.js
@@ -23,3 +23,72 @@ it('renders the form elements correctly', () => {
   });
 
 
+  test('should show error message when password less than eight Characters', async () => {
+    const { getByLabelText, getByText } =   render(
+        <BrowserRouter>
+          <SignupForm />
+        </BrowserRouter>
+      );
+    const Input = screen.getByPlaceholderText('Password');
+    fireEvent.change(Input, { target: { value: 'abc' } });
+    fireEvent.submit(Input);
+    await waitFor(() =>
+      expect(getByText('Must be at least 8 characters')).toBeInTheDocument()
+    );
+  });
+
+  test('should show error message when password does have number or special character', async () => {
+    const { getByLabelText, getByText } =   render(
+        <BrowserRouter>
+          <SignupForm />
+        </BrowserRouter>
+      );
+    const Input = screen.getByPlaceholderText('Password');
+    fireEvent.change(Input, { target: { value: 'abcdefghi' } });
+    fireEvent.submit(Input);
+    await waitFor(() =>
+      expect(getByText('Password must contain a Number, Special, Uppercase, and Lowercase Character')).toBeInTheDocument()
+    );
+  });
+
+  test('should show error message when confirm password does not match with password', async () => {
+    const { getByLabelText, getByText } =   render(
+        <BrowserRouter>
+          <SignupForm />
+        </BrowserRouter>
+      );
+    const Input = screen.getByPlaceholderText('Confirm Password');
+    fireEvent.change(Input, { target: { value: 'abcdefghi' } });
+    fireEvent.submit(Input);
+    await waitFor(() =>
+      expect(getByText('Passwords Do Not Match')).toBeInTheDocument()
+    );
+  });
+
+  test('should show error message when Email is not correct format', async () => {
+    const { getByLabelText, getByText } =   render(
+        <BrowserRouter>
+          <SignupForm />
+        </BrowserRouter>
+      );
+    const Input = screen.getByPlaceholderText('Email');
+    fireEvent.change(Input, { target: { value: 'say' } });
+    fireEvent.submit(Input);
+    await waitFor(() =>
+      expect(getByText('Invalid Email Address')).toBeInTheDocument()
+    );
+  });
+
+  test('should show error message when confirm Email does not match with email', async () => {
+    const { getByLabelText, getByText } =   render(
+        <BrowserRouter>
+          <SignupForm />
+        </BrowserRouter>
+      );
+    const Input = screen.getByPlaceholderText('Confirm Email');
+    fireEvent.change(Input, { target: { value: 'invalid' } });
+    fireEvent.submit(Input);
+    await waitFor(() =>
+      expect(getByText('Emails Do Not Match')).toBeInTheDocument()
+    );
+  });
