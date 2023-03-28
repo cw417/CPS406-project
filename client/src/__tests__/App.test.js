a947df923f7392cdc788dc3aaf530fd3dcd7dc2f
cw417 <chriswhitlock417@gmail.com>
2023-03-27 21:52:10 -0400

    Begin writing testing

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+import AddContact from '../comps/AddContact';
+import AddPayee from '../comps/AddPayee';
+import App from '../App'
+import { BrowserRouter } from 'react-router-dom';
+import Homepage from '../pages/Homepage';
+
+/* SETUP */
+
+// need to mock useNavigate to deal with jest issues
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...(jest.requireActual('react-router-dom')),
+  useNavigate: () => mockedUsedNavigate
+}));
+
+/* TESTS */
+
+test('renders App', async () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+});
+
+test('renders Homepage', async () => {
+  render(
+    <BrowserRouter>
+      <Homepage />
+    </BrowserRouter>
+  );
+  expect(screen.getByText('The Reserve')).toBeInTheDocument();
+  // expect(mockedUsedNavigate).toHaveBeenCalledWith('/post-1');
+});
+
+test('renders AddContact component', () => {
+  render(<AddContact />);
+});
+
+test('renders AddPayee component', () => {
+  render(<AddPayee />);
+});
