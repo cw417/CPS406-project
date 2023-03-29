41303a3bfd7bca02d4ff51d2ae6376387dda4c55
cw417 <chriswhitlock417@gmail.com>
2023-03-29 16:35:19 -0400

    Work on testing for Dashboard page

---
+++ client/src/__tests__/Dashboard.test.js
@@ -0,0 +1,15 @@
+import { render, fireEvent, screen, renderHook } from "@testing-library/react";
+import Dashboard from "../pages/Dashboard";
+import AccountsOverview from "../comps/AccountsOverview";
+import QuickActions from "../comps/QuickActions";
+import { BrowserRouter } from 'react-router-dom';
+import { act } from "react-dom/test-utils";
+
+  it('renders Dashboard page', () => {
+    render(<BrowserRouter><Dashboard /></BrowserRouter>);
+  });
+
+  it('renders Loading message if customer is null', () => {
+    render(<BrowserRouter><Dashboard /></BrowserRouter>);
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
